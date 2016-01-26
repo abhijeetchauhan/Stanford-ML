62f525ffa411d9a74eba4a92b0dbf6e0a8acdedd
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2016-01-26 15:35:14 +0530

    Solution for Assignment:  Support Vector Machines

--- Week 7/ex6/dataset3Params.m
+++ Week 7/ex6/dataset3Params.m
@@ -10,7 +10,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
-
+error=100000;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,8 +22,30 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
+%{
+model=svmTrain(X,y,C,@(x1,x2) gaussianKernel(x1,x2,sigma));
+predictions=svmPredict(model,Xval);
+error=mean(double(predictions ~= yval));
+fprintf("C-%f,sigma-%f,error-%f\n",C,sigma,error);
+guess=[0.01;.03;.1;.3;1;3;10;30];
+for i=1:8
+  for j=1:8
+    %[model]=svmTrain(X,y,guess(i),@gaussianKernel);
+    model=svmTrain(X,y,guess(i),@(x1,x2) gaussianKernel(x1,x2,guess(j)));
+    predictions=svmPredict(model,Xval);
+    me=mean(double(predictions ~= yval));
+    fprintf("C-%f,sigma-%f,error-%f,mean-%f\n",C,sigma,error,me);
+    if(me<error)
+      error=me;
+      C=guess(i);
+      sigma=guess(j);
+      fprintf("C-%f,sigma-%f,error-%f\n",C,sigma,error);
+    endif
+  endfor
+endfor
+%}
+% After running above code answers came C=1 sigma=0.1
+C=1;sigma=0.1;
 
 
 
